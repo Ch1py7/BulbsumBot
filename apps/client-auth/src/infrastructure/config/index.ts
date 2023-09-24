9db19b815f62d28be08fb120500a0b629d96ab6f
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-09-23 18:54:13 -0600

    feat: add config client

---
+++ apps/client-auth/src/infrastructure/config/index.ts
@@ -0,0 +1,7 @@
+export const config = {
+  channel: '#bulbsum',
+  account: 'bulbsum',
+  password: process.env.OAUTH_TOKEN,
+  bot_client_id: process.env.BOT_CLIENT_ID,
+  bot_secret: process.env.BOT_SECRET,
+}
