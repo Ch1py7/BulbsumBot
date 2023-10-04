cf91cc68a50702260724734b98e75a4ff682d590
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-10-03 19:20:37 -0600

    feat: add dbhandler and admin from firebase in awilix container

--- apps/bulbot/src/container.ts
+++ apps/bulbot/src/container.ts
@@ -3,12 +3,16 @@ import { InjectionMode, asClass, asValue, createContainer } from 'awilix'
 import { GetAccountInfo } from 'application/getAccountInfo'
 import { GetOAuthToken } from 'application/getOAuthToken'
 import { RefreshToken } from 'application/refreshToken'
+// Infrastructure config
+import { config } from 'infrastructure/config'
 // Persistance
 import { TwitchBot } from 'infrastructure/irc/twitchBot'
 import { TwitchRepository } from 'infrastructure/persistance/twitch/twitchRepository'
+import { FirebaseHandler } from 'infrastructure/persistance/firebase/dbHandler'
 // Services
 import { ParseMessage } from 'infrastructure/services'
 // Libraries
+import admin from 'firebase-admin'
 import axios from 'axios'
 import http from 'http'
 import socketio from 'socket.io'
@@ -24,14 +28,19 @@ container.register({
   getOAuthToken: asClass(GetOAuthToken),
   refreshToken: asClass(RefreshToken),
 
+  // Infrastructure config
+  config: asValue(config),
+
   // Persistance
   twitchBot: asClass(TwitchBot),
   twitchRepository: asClass(TwitchRepository),
+  dbHandler: asClass(FirebaseHandler),
 
   // Services
   parseMessage: asClass(ParseMessage),
 
   // Libraries
+  admin: asValue(admin),
   websocket: asValue(websocket),
   axios: asValue(axios),
   socketIo: asValue(socketio),
--- apps/bulbot/src/types/container.d.ts
+++ apps/bulbot/src/types/container.d.ts
@@ -5,9 +5,13 @@ import type { RefreshToken } from 'application/refreshToken'
 // Persistance
 import type { TwitchBot } from 'infrastructure/irc/twitchBot'
 import type { TwitchRepository } from 'infrastructure/persistance/twitch/twitchRepository'
+import type { FirebaseHandler } from 'src/infrastructure/persistance/firebase/dbHandler'
+// Infrastructure
+import type { config } from 'infrastructure/config'
 // Services
 import type { ParseMessage } from 'infrastructure/services'
 // Libraries
+import type admin from 'firebase-admin'
 import type axios from 'axios'
 import type http from 'http'
 import type socketio from 'socket.io'
@@ -20,15 +24,20 @@ declare global {
     getAccountInfo: GetAccountInfo
     getOAuthToken: GetOAuthToken
     refreshToken: RefreshToken
+
+    // Infrastructure config
+    config: typeof config
   
     // Persistance
     twitchBot: TwitchBot
     twitchRepository: TwitchRepository
+    dbHandler: FirebaseHandler
 
     // Services
     parseMessage: ParseMessage
   
     // Libraries
+    admin: typeof admin
     websocket: typeof websocket
     message: Message
     axios: typeof axios
