32abbd0da6c6c4ca26659de32c22b3517f64712c
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-10-03 19:13:22 -0600

    feat: add database config

---
+++ apps/bulbot/src/infrastructure/config/index.ts
@@ -0,0 +1,10 @@
+export const config = {
+  firebaseConfig: {
+    credential: {
+      projectId: process.env.FIREBASE_PROJECT_ID!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+    },
+    databaseURL: process.env.FIREBASE_DATABASE_URL!,
+  }
+}
