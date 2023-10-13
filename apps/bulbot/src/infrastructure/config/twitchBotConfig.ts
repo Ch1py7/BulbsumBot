cf7128eba32b0200c24aa020032f2df120dd70d2
Ch1py7 <garcialopezgerardo6612@gmail.com>
2023-10-13 12:57:32 -0600

    feat: add configuration files

---
+++ apps/bulbot/src/infrastructure/config/twitchBotConfig.ts
@@ -0,0 +1,6 @@
+export const twitchBotConfig = {
+  initialToken: process.env.BOT_INITIAL_TOKEN,
+  initialRefreshToken: process.env.BOT_INITIAL_REFRESH_TOKEN,
+  clientId: process.env.BOT_CLIENT_ID,
+  clientSecret: process.env.BOT_SECRET
+}
